7b6e483f3709a7f1d7f1b1add309049a319e3626
Lucas Kauã <lucaskauajeronimo@gmail.com>
2025-05-08 20:02:03 +0000

    embora

--- project_flask/main.py
+++ project_flask/main.py
@@ -4,9 +4,19 @@ Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
+       return render_templates('index.html')
 
+@app.route('/usuario/<usuario>')
+def usuario(nome):
+       return nome
+
+@app.route('/contatos')
+def index():
+       return render_templates('contatos.html',
+       nome="lucas",
+       email="adm.adm@gmail.com")
+def index():
        return 'Olá Mundo!'
 
 if __name__ == '__main__':
