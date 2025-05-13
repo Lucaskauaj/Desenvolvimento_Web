6600691ed315df8a9b5ba92022511756a9cc36dd
Lucas <lucaskauajeronimo@gmail.com>
2025-05-13 20:58:04 -0300

    ok

--- project_flask/main.py
+++ project_flask/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-       return render_templates('index.html')
+       return render_template('index.html')
 
 @app.route('/usuario/<usuario>')
 def usuario(nome):
@@ -12,7 +12,7 @@ def usuario(nome):
 
 @app.route('/contatos')
 def index():
-       return render_templates('contatos.html',
+       return render_template('contatos.html',
        nome="lucas",
        email="adm.adm@gmail.com")
 def index():
